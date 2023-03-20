710531d6ace0c0144f70c43123cbf469af66dfff
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-03-19 21:37:57 -0600

    Implementación para mostrar por snack.open

--- sistema-examenes-frontend/src/app/pages/signup/signup.component.ts
+++ sistema-examenes-frontend/src/app/pages/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'; //42.1-Esto se import
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { UserService } from 'src/app/services/user.service';
 
 
@@ -12,12 +13,12 @@ export class SignupComponent implements OnInit {
 
   //42.1-Agrego metodo
   public user = {
-    username : '',
-    password : '',
-    nombre : '',
-    apellido : '',
-    email : '',
-    telefono : ''
+    username: '',
+    password: '',
+    nombre: '',
+    apellido: '',
+    email: '',
+    telefono: ''
   }
 
   ngOnInit(): void {
@@ -26,13 +27,20 @@ export class SignupComponent implements OnInit {
 
   //42.2-Importante agregar constructor
   //y lo de UserService
-  constructor(private userService: UserService) { }
+  //46.- Inyectamos el Snack: MatSnackBar
+  constructor(private userService: UserService, private snack: MatSnackBar) { }
 
   //42.3-Agregando condicional
   formSubmit() {
     console.log(this.user);
     if (this.user.username == '' || this.user.username == null) {
-      alert('El nombre de usuario es requerida');
+      //47.- Para mostrar la ventana
+      this.snack.open("¡¡El nombre de usuario es requerido!!", "Aceptar", {
+        duration: 3000,
+        verticalPosition: "top",
+        horizontalPosition: "right"
+      });
+      //45.2-[Quitamos]->alert('El nombre de usuario es requerida');
       return;
     }
 
@@ -43,7 +51,12 @@ export class SignupComponent implements OnInit {
         alert('¡Usuario guardado con exito!')
       }, (error) => {
         console.log(error);
-        alert('Ha ocurrido un error en el sistema');
+        //47.1-[Quitamos]->alert('Ha ocurrido un error en el sistema');
+        //48.- Para mostrar la ventana en caso de error
+        this.snack.open("¡¡Ha ocurrido un error en el sistema!!", "Aceptar", {
+          duration: 3000
+        });
+        
       }
 
     )
