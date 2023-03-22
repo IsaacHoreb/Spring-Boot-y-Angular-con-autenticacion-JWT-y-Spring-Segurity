5639b99a1c81e07f10309b1e27cd553eea0b0f87
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-03-22 11:28:00 -0600

    Agregado de anotacion @Service (Se me olvido ponerla)

--- sistema-examenes-backend/src/main/java/com/example/examenes/services/impl/UserDetailsServiceImpl.java
+++ sistema-examenes-backend/src/main/java/com/example/examenes/services/impl/UserDetailsServiceImpl.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 //74.-Implementamos lo siguiente
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService { //Sirve para cargar un User
     @Autowired
     private UsuarioRepository usuarioRepository;
