3089e36cd127cd7ec1df86352b4fb711cef5ff37
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-03-23 20:35:43 -0600

    Validación de campos de inicio de sección

--- sistema-examenes-frontend/src/app/pages/login/login.component.ts
+++ sistema-examenes-frontend/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -8,24 +9,41 @@ import { Component, OnInit } from '@angular/core';
 
 //99. Agreamos e implementamos todo lo sig.
 export class LoginComponent implements OnInit {
-  
+
   //100.-
   loginData = {
-    "username" : "",
-    "password" : ""
+    "username": "",
+    "password": ""
   }
 
-  //101.-
-  constructor() {}
+  //101.-Creamos
+  //107.-Ingresamos
+  constructor(private snack: MatSnackBar) { }
 
   //Se agrega al implementar
   ngOnInit(): void {
-   
+
   }
 
   //102.-
-  formSubmit(){
-    console.log("Click en el boton de login")
+  formSubmit() {
+    //107.1-QUITAR
+    //console.log("Click en el boton de login")
+    //108.-Condicion agregando
+    if (this.loginData.username == "" || this.loginData.username == null) {
+      this.snack.open("El nombre de usuario es requerido", "Aceptar", {
+        duration: 3000
+      })
+      return;
+    }
+
+    //109.-Codicion si falta el password y probamos la aplicación 
+    if (this.loginData.password == "" || this.loginData.password == null) {
+      this.snack.open("La constraseña es requerido", "Aceptar", {
+        duration: 3000
+      })
+      return;
+    }
   }
 
   //103.-Nos vamos al login.component.html... en el <form action="">
